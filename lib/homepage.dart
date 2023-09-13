4de391ebdbd6f12c504ec9420deef90f5f57afe3
Rohan R. Venugopal <rohanaathira22@gmail.com>
2023-09-14 00:05:50 +0530

    Added icons on top right of home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
+  Widget HomeAppBar(String message){
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation:0.0,
+      title: Text(message),actions:  [Icon(Icons.notification_add,),Icon(Icons.timer),Padding(padding:EdgeInsets.only(right: 10), child:Icon(Icons.settings))],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Container(
+      child:Column(children: [
+        HomeAppBar('Home')
+      ],),
       decoration:BoxDecoration(
         gradient: LinearGradient(colors: [Color.fromARGB(255, 44, 122, 160),Colors.black],begin: Alignment.topRight,end: Alignment.bottomLeft)
       )
